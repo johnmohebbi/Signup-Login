79c2f0571da8097c9b246b2974f5d3df3a5d9fe7
john Mohebbi <mohebbijohn@gmail.com>
2021-12-03 11:54:16 +0330

    added form inputs to signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,7 +3,32 @@ import React from 'react';
 const Signup = () => {
     return (
         <div>
-            
+            <form>
+                <div>
+                    <label>username</label>
+                    <input type="text"  />
+                </div>
+                <div>
+                    <label>email</label>
+                    <input type="email"  />
+                </div>
+                <div>
+                    <label>password</label>
+                    <input type="password"  />
+                </div>
+                <div>
+                    <label>confirmpassword</label>
+                    <input type="password"  />
+                </div>
+                <div>
+                    <label>your accept our rules</label>
+                    <input type="checkbox"  />
+                </div>
+                <div>
+                    <a href="/#">login</a>
+                    <button type="submit">signUp</button>
+                </div>
+            </form>
         </div>
     );
 };
